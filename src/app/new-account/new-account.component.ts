568661be0f0d3e27bbc6d78b49de4c2d32bdda5a
Rachel Kat <rachel.kat@koho.ca>
2020-12-26 16:18:46 -0500

    the wrong way to instantiate a service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-
+import { Component, EventEmitter, Output } from "@angular/core";
 @Component({
-  selector: 'app-new-account',
-  templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  selector: "app-new-account",
+  templateUrl: "./new-account.component.html",
+  styleUrls: ["./new-account.component.css"],
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
-      status: accountStatus
+      status: accountStatus,
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    // const service = new LoggingService(); // THIS IS WRONG
+    // service.logStatusChange(accountStatus); // THIS IS WRONG
+    // console.log("A server status changed, new status: " + accountStatus);
   }
 }
